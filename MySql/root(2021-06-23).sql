5670d43dc9117bb3437c600f9eaaea3c3d56630d
rudgjs8080 <kkh_8080@naver.com>
2021-06-24 16:55:23 +0900

    0624com

--- MySql/root(2021-06-23).sql
+++ MySql/root(2021-06-23).sql
@@ -16,29 +16,25 @@ values('20210001','손흥민','체육학과','2','010-1234-1234');
 
 select * from tbl_student;
 select * from tbl_score;
+select * from tbl_subject;
+
+
 create table tbl_score(
 sc_seq	bigint	auto_increment	primary key,
 sc_stnum	char(8)	NOT NULL	,
 sc_sbcode	char(4)	NOT NULL,	
 sc_score	int	NOT NULL	
 );
--- drop table tbl_score;
-insert into tbl_score(sc_stnum, sc_sbcode, sc_score)
-values ('20210001' , 'S001', '90');
+drop table tbl_student;
 
-insert into tbl_score(sc_stnum, sc_sbcode, sc_score)
-values ('20210001' , 'S002', '60');
 
-insert into tbl_score(sc_stnum, sc_sbcode, sc_score)
-values ('20210001' , 'S003', '91');
 
 create table tbl_subject(
 sb_code	char(4)		primary key,
 sb_name	varchar(20)	NOT NULL	,
 sb_prof	varchar(20)		
 );
+select * from tbl_student;
 
-insert into tbl_subject(sb_code, sb_name, sb_prof)
-values ('S001','국어','기다은');
 
 commit;
