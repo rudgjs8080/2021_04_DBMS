ffd4700049f7e533ad113062caf51641c43fd5f6
rudgjs8080 <kkh_8080@naver.com>
2021-06-25 17:35:42 +0900

    0625com

--- MySql/Root(2021-05-18).sql
+++ MySql/Root(2021-05-18).sql
@@ -52,4 +52,3 @@ flush privileges;
 
 
 
-
--- MySql/root(2021-06-23).sql
+++ MySql/root(2021-06-23).sql
@@ -36,5 +36,73 @@ sb_prof	varchar(20)
 );
 select * from tbl_student;
 
+/*
+tbl_subject, tbl_score table을 가지고
+각 학생의 성적 리스트를 출력해보기
+과목 리스트를 출력하고, 각 과목의 성적이 입력된 학생의 리스트를 확인하기
+
+학번을 조건으로 하여 한 학생의 성적입력 여부를 확인하기
+*/
+
+-- subquery 이용 방법
+select SB.sb_code, SB.sb_name, SB.sb_prof, SC.sc_stnum, SC.sc_score
+from tbl_subject SB
+	left join 
+    (select * from tbl_score where sc_stnum = '20210001') SC
+		on SC.sc_sbcode = SB.sb_code
+where SC.sc_stnum = '20210001';
+
+select SB.sb_code, SB.sb_name, SB.sb_prof, SC.sc_stnum, SC.sc_score
+from tbl_subject SB
+	left join tbl_score SC
+		on SC.sc_sbcode = SB.sb_code
+        and SC.sc_stnum = '20210001' limit 5;
+        
+select SB.sb_code, SB.sb_name, SB.sb_prof, SC.sc_stnum, SC.sc_score
+from tbl_subject SB
+	left join tbl_score SC
+		on SC.sc_sbcode = SB.sb_code;
+/*
+과목리스트를 전체 보여주고, 학생의 성적 table을 JOIN하여
+학생의 점수가 있으면 점수를 보이고
+없으면 null로 보여주는 JOIN SQL 문
+
+이 JOIN 명령문에
+특정한 학번을 조건으로 부여하여
+한 학생의 성적여부를 조회하는 SQL 만들기
+
+순수한 JOIN을 사용하여 한 학생의 성적을 조회하는데
+학생의 성적이 tbl_score table에 있으면 점수를 보이고
+없으면 null로 표현하기 위하여 
+WHERE 절에서 학번을 조건으로 부여하지 않고
+JOIN문의 ON 절에 학번을 조건으로 부여한다
+
+*/
+select SB.sb_code, SB.sb_name, SB.sb_prof, SC.sc_stnum, SC.sc_score
+from tbl_subject SB
+	left join tbl_score SC
+		on SC.sc_sbcode = SB.sb_code
+        and SC.sc_stnum = '20210001' limit 5;
+        
+select * from tbl_student;
+
+select count(*) from tbl_score
+where sc_stnum = '1';
+
+select * from tbl_score;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 commit;
