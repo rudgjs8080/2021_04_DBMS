bd2c36594372d0559223ce92afd55c35e40e1880
rudgjs8080 <kkh_8080@naver.com>
2021-07-05 00:31:36 +0900

    0704home

---
+++ MySql/woo(2021-07-04).sql
@@ -0,0 +1,11 @@
+create database woo;
+use woo;
+
+create table tbl_user(
+us_name varchar(50),
+us_id varchar(50),
+us_pw varchar(50),
+us_city varchar(50),
+us_dist varchar(50));
+
+select * from tbl_user;
